4c2fb046f0e8c06c7acdd47329f1c5b3a478cf74
km-saifullah <kmsaifullah16@gmail.com>
2024-09-05 12:55:55 +0600

    login route added

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import adminRoute from './routes/adminRoute.js'
+import { signIn } from './controllers/authController.js'
 
 const app = express()
 
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// auth routes
+app.use('/api/v1/admin', signIn)
 
 // routes
 app.use('/api/v1/admin', adminRoute)
