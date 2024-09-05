38c73f887aaf5262a21b75e5ad34a53d61c51e97
km-saifullah <kmsaifullah16@gmail.com>
2024-09-05 12:01:23 +0600

    login feature done

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,8 +1,9 @@
 import apiResponse from 'quick-response'
 import Admin from '../models/adminModel.js'
+import { signToken } from '../utils/signToken.js'
 
-
-
+// @desc  Admin Signin
+// @route POST /api/v1/admin/login
 export const signIn = async (req, res, next) => {
   try {
     const { email, password } = req.body
@@ -16,15 +17,21 @@ export const signIn = async (req, res, next) => {
 
     // check if the admin exist or not
     const admin = await Admin.findOne({ email: email })
+
     if (!admin || (await admin.correctPassword(password, admin.password))) {
       return res
-        .status(400)
-        .json(apiResponse(400, 'Incorrect email or password'))
+        .status(401)
+        .json(apiResponse(401, 'Incorrect email or password'))
     }
+
+    // send token to the admin
+    const token = signToken(admin._id)
 
     return res
       .status(200)
-      .json(apiResponse(200, 'Login Susscessful', { admin }))
+      .json(
+        apiResponse(200, 'Login Susscessful', { admin: admin, token: token })
+      )
   } catch (error) {
     return res.status(500).json({ status: 'fail', message: `${error.message}` })
   }
